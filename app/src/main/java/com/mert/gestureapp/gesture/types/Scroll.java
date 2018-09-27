4cf665a18f725818d8d65e44e8679b3d7d64e76d
mert <merttutsak@gmail.com>
2018-09-28 00:54:37 +0300

    Scroll is corrected

--- app/src/main/java/com/mert/gestureapp/gesture/types/Scroll.java
+++ app/src/main/java/com/mert/gestureapp/gesture/types/Scroll.java
@@ -1,6 +1,7 @@
 package com.mert.gestureapp.gesture.types;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.MotionEvent;
 
 import com.mert.gestureapp.gesture.GestureListener;
@@ -15,9 +16,36 @@ public class Scroll extends GesturePresenter {
     }
 
     @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        scroll.BottomScroll();
-        return super.onScroll(e1, e2, distanceX, distanceY);
+    public boolean onScroll(MotionEvent downEvent, MotionEvent moveEvent, float distanceX, float distanceY) {
+        boolean result = false;
+        float diffY = moveEvent.getY() - downEvent.getY();
+        float diffX = moveEvent.getX() - downEvent.getX();
+
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            if (Math.abs(diffX) > SWIPE_THRESHOLD_VELOCITY && Math.abs(distanceX) > SWIPE_THRESHOLD_VELOCITY) {
+                if (diffX > 0) {
+                    Log.d("GESTURE_EVENT", "onFling => Right");
+                    scroll.RightScroll();
+                } else {
+                    Log.d("GESTURE_EVENT", "onFling => Left");
+                    scroll.LeftScroll();
+                }
+                result = true;
+            }
+        } else {
+            if (Math.abs(diffY) > SWIPE_THRESHOLD_VELOCITY && Math.abs(distanceY) > SWIPE_THRESHOLD_VELOCITY) {
+                if (diffY > 0) {
+                    Log.d("GESTURE_EVENT", "onFling => Bottom");
+                    scroll.BottomScroll();
+                } else {
+                    Log.d("GESTURE_EVENT", "onFling => Top");
+                    scroll.TopScroll();
+                }
+                result = true;
+            }
+        }
+
+        return result;
     }
 }
 
